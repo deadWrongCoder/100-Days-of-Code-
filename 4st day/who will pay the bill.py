ccd14b2d332dfc3f29b82d19a7412bc69395d4bc
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-13 12:48:10 +0300

    Simpyfing the solution by using built-in function

--- 4st day/who will pay the bill.py
+++ 4st day/who will pay the bill.py
@@ -1,5 +1,4 @@
 import random
 
-names = input("Type all the names divided by a comma and a space. ").split(", ")
-length = len(names)
-print(names[random.randint(0, length - 1)] + " is going to pay the total bill! ")
+names = input("Type the names divided by a comma and a space. ").split(", ")
+print(random.choice(names) + " is going to pay the total bill! ")
