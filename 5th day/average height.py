427f8885d630a55dd49919f3c22d3dcb52994db0
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-13 15:34:52 +0300

    Update average height.py

--- 5th day/average height.py
+++ 5th day/average height.py
@@ -1,5 +1,7 @@
-heights = [162, 170, 185, 174, 173, 181]
+heights = input("What are heights? ").split(", ")
 sum = 0
+for n in range(0, len(heights)):
+  heights[n] = int(heights[n])
 for height in heights:
   sum += height
 print(f"Average height is {round(sum/len(heights), 2)}")  
