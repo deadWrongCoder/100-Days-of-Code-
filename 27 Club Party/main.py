f0ae8753d3f1766eac97093f898048320f9af85b
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-19 12:44:29 +0300

    Update main.py

--- 27 Club Party/main.py
+++ 27 Club Party/main.py
@@ -4,19 +4,20 @@ username = "(name)"
 
 with open("names/names.txt") as file:
   names = file.readlines()
-
+  for i in range(0, len(names)):
+    names[i] = names[i].strip()
 
 with open("sample/invitation.txt") as file:
-  invitation = file.readlines()
+  invitation = file.read()
 
 filenames = []
 for i in range(0, len(names)):
   filenames.append(names[i] + ".txt")
   
 for i in range(0, len(names)):
-  invitation[0] = invitation[0].replace(username, names[0])
-  
-
+  new_invitation = invitation.replace(username, names[i])
   with open(filenames[i], "w") as file:
-    file.write(" ".join(invitation))
+    file.write(new_invitation)
+  
+  
   
