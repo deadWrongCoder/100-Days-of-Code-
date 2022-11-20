20a960510d1f2f3721a9a7c548ed2853f182c0ee
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-20 14:50:53 +0300

    Update main.py

--- 25 day/main.py
+++ 25 day/main.py
@@ -4,3 +4,4 @@ import pandas
 data = pandas.read_csv("weather.csv")
 print("Max value is ", data["temp"].max())
 print("Mean value is ", round(data["temp"].mean(), 1))
+print(data[data.temp == data.temp.max()])
