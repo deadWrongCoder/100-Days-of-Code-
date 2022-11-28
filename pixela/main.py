75135529aab5c5bede4ec9fc6b15e975797b3ffc
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-28 16:41:39 +0300

    Update main.py

--- pixela/main.py
+++ pixela/main.py
@@ -10,7 +10,7 @@ graph_endpoint = f"{endpoint}/{USERNAME}/graphs/graph1"
 today = datetime.now()
 date = today.strftime("%Y%m%d")
 graph_config = {
-    "date": "20221128",
+    "date": date,
     "quantity": "2.0"
 }
 
@@ -20,4 +20,5 @@ headers = {
 
 response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
+
 
