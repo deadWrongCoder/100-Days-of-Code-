a4a6a5936397cc7956c4b43917ef4ba293ca0a39
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-28 16:41:06 +0300

    Create main.py

---
+++ pixela/main.py
@@ -0,0 +1,23 @@
+import requests
+
+from datetime import  datetime
+
+USERNAME = "xenia"
+TOKEN = "secret"
+endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{endpoint}/{USERNAME}/graphs/graph1"
+
+today = datetime.now()
+date = today.strftime("%Y%m%d")
+graph_config = {
+    "date": "20221128",
+    "quantity": "2.0"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
+
