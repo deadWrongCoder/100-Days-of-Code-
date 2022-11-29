899ee88990ca70abb1a41fe7c6c19c2edc3e8168
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-29 16:48:53 +0300

    Update main.py

--- BeautifulSoup practice/main.py
+++ BeautifulSoup practice/main.py
@@ -14,7 +14,14 @@ article_links = [value.find(name="a")["href"] for value in articles]
 article_scores = [score.getText() for score in soup.find_all(name="span", class_="score")]
 
 article_points = [int(score.split(" ")[0]) for score in article_scores]
+
+largest_score = max(article_points)
+index = article_points.index(largest_score)
 print(article_names)
 print(article_links)
 print(article_scores)
 print(article_points)
+print(largest_score)
+print(article_names[index])
+
+
