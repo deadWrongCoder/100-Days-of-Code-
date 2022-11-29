4bc4c79be5eeb01fd15b38dc3dcb687c6fc04483
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-11-29 16:31:28 +0300

    Update main.py

--- BeautifulSoup practice/main.py
+++ BeautifulSoup practice/main.py
@@ -4,5 +4,12 @@ import requests
 response = requests.get(url="https://news.ycombinator.com/")
 page = response.text
 soup = BeautifulSoup(page, "html.parser")
-print(soup.find(name="span", class_="titleline").getText())
-print(soup.find(name="span", class_="score").getText())
+article_tag = soup.find(name="span", class_="titleline")
+article_name = article_tag.find(name="a").getText()
+article_link = article_tag.find(name="a")["href"]
+article_score = soup.find(name="span", class_="score").getText()
+print(article_tag)
+print(article_name)
+print(article_link)
+print(article_score)
+
