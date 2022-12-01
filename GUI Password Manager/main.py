d909c6c9fe7839e0d840a34652b5b0fe425845ba
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-01 11:38:07 +0300

    Create layout

---
+++ GUI Password Manager/main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+window = Tk()
+
+window.config(padx=20, pady=20)
+window.title("Password Manager")
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+website = Label(text="Website: ")
+website.grid(row=1, column=0)
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+username = Label(text="Username/email: ")
+username.grid(row=2, column=0)
+username_entry = Entry(width=35)
+username_entry.grid(row=2, column=1, columnspan=2)
+
+password = Label(text="Password: ")
+password.grid(row=3, column=0)
+password_entry = Entry(width=23)
+password_entry.grid(row=3, column=1)
+generate_button = Button(text="Generate")
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=34)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
